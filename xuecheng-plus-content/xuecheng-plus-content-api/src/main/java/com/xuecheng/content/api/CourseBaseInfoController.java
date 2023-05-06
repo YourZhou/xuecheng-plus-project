02fa92699a6085e274cfa41ed6f1132ad0ca5bcc
冯逸洲 <362409381@qq.com>
2023-05-06 18:32:49 +0800

    111

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -0,0 +1,27 @@
+package com.xuecheng.content.api;
+
+import com.xuecheng.base.model.PageParams;
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.QueryCourseParamsDto;
+import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YiZhou
+ * @date 2023/5/6
+ * @Description
+ */
+@Api(value = "课程信息管理接口",tags = "课程信息管理接口")
+@RestController
+public class CourseBaseInfoController {
+
+    @ApiOperation("课程查询接口")
+    @PostMapping("/course/list")
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto) {
+        return null;
+    }
+}
---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,27 @@
+package com.xuecheng.content.model.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author YiZhou
+ * @date 2023/5/6
+ * @Description
+ */
+@Data
+@ToString
+public class QueryCourseParamsDto {
+
+    /**
+     * 审核状态
+     */
+    private String auditStatus;
+    /**
+     * 课程名称
+     */
+    private String courseName;
+    /**
+     * 发布状态
+     */
+    private String publishStatus;
+}
